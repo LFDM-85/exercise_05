751c60681403575e85c08ce19cc35c4b64a321c0
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-03-31 15:28:59 +0100

    Exercise 5

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
 "use strict";
 console.clear();
+const divElement = document.querySelector("#persons");
 const maleNames = ["Antonio", "Manuel", "Nuno"];
 const femaleNames = ["Maria", "Ana", "Carolina"];
 const maleAges = [43, 15, 22];
 const femaleAges = [35, 17, 5];
+// Sorted Logic
 const merge = () => {
-    const femaleArray = femaleNames.map((val, index) => [val, femaleAges[index]]);
-    // console.log(femaleArray);
-    const femaleObj = Object.fromEntries(femaleArray);
-    // console.log(femaleObj);
-    const maleArray = maleNames.map((val, index) => [val, maleAges[index]]);
-    // console.log(maleArray);
-    const maleObj = Object.fromEntries(maleArray);
-    // console.log(maleObj);
-    const mergeObj = { ...femaleObj, ...maleObj };
-    console.log(mergeObj);
-    return mergeObj;
+    const person = [];
+    femaleNames.map((val, index) => {
+        person.push({
+            firstName: val,
+            age: femaleAges[index],
+        });
+    });
+    maleNames.map((val, index) => {
+        person.push({
+            firstName: val,
+            age: maleAges[index],
+        });
+    });
+    console.log(person);
+    const adultTitle = document.createElement("h3");
+    adultTitle.innerText = "Adults (Ages >= 18 years)";
+    const listDivAdult = document.createElement("div");
+    listDivAdult.appendChild(adultTitle);
+    divElement.appendChild(listDivAdult);
+    const childTitle = document.createElement("h3");
+    childTitle.innerText = "Children (Ages < 18 years)";
+    const listDivChild = document.createElement("div");
+    listDivChild.appendChild(childTitle);
+    divElement.appendChild(listDivChild);
+    const adults = person
+        .filter((_, i) => person[i].age >= 18)
+        .forEach((adult) => {
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+        const names = document.createTextNode(adult.firstName);
+        li.appendChild(names);
+        ul.appendChild(li);
+        listDivAdult.appendChild(ul);
+    });
+    const children = person
+        .filter((_, i) => person[i].age < 18)
+        .forEach((children) => {
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+        const names = document.createTextNode(children.firstName);
+        li.appendChild(names);
+        ul.appendChild(li);
+        listDivChild.append(ul);
+    });
 };
 merge();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 console.clear();
+const divElement = document.querySelector("#persons")!;
 
 const maleNames = ["Antonio", "Manuel", "Nuno"];
 const femaleNames = ["Maria", "Ana", "Carolina"];
@@ -6,19 +7,65 @@ const femaleNames = ["Maria", "Ana", "Carolina"];
 const maleAges = [43, 15, 22];
 const femaleAges = [35, 17, 5];
 
-const merge = () => {
-  const femaleArray = femaleNames.map((val, index) => [val, femaleAges[index]]);
-  // console.log(femaleArray);
-  const femaleObj = Object.fromEntries(femaleArray);
-  // console.log(femaleObj);
-  const maleArray = maleNames.map((val, index) => [val, maleAges[index]]);
-  // console.log(maleArray);
-  const maleObj = Object.fromEntries(maleArray);
-  // console.log(maleObj);
+interface Obj {
+  firstName: string;
+  age: number;
+}
 
-  const mergeObj = { ...femaleObj, ...maleObj };
-  console.log(mergeObj);
-  return mergeObj;
+// Sorted Logic
+const merge = () => {
+  const person: Obj[] = [];
+  femaleNames.map((val, index) => {
+    person.push({
+      firstName: val,
+      age: femaleAges[index],
+    });
+  });
+
+  maleNames.map((val, index) => {
+    person.push({
+      firstName: val,
+      age: maleAges[index],
+    });
+  });
+
+  console.log(person);
+
+  const adultTitle = document.createElement("h3");
+  adultTitle.innerText = "Adults (Ages >= 18 years)";
+
+  const listDivAdult = document.createElement("div");
+
+  listDivAdult.appendChild(adultTitle);
+  divElement.appendChild(listDivAdult);
+
+  const childTitle = document.createElement("h3");
+  childTitle.innerText = "Children (Ages < 18 years)";
+  const listDivChild = document.createElement("div");
+  listDivChild.appendChild(childTitle);
+  divElement.appendChild(listDivChild);
+
+  const adults = person
+    .filter((_, i) => person[i].age >= 18)
+    .forEach((adult) => {
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+      const names = document.createTextNode(adult.firstName);
+      li.appendChild(names);
+      ul.appendChild(li);
+      listDivAdult.appendChild(ul);
+    });
+
+  const children = person
+    .filter((_, i) => person[i].age < 18)
+    .forEach((children) => {
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+      const names = document.createTextNode(children.firstName);
+      li.appendChild(names);
+      ul.appendChild(li);
+      listDivChild.append(ul);
+    });
 };
 
 merge();
